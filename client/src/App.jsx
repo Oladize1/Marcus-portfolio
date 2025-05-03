4113912174eb5bf345235cc763a97c0f8e03d8ae
Pamilerin Marcus Oladunjoye <pamoladize10@gmail.com>
2025-05-03 02:06:43 +0100

    Feat: Add About me Page and run build for host on Netlify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './Pages/HomePage.jsx'
+import AboutMePage from './Pages/AboutMePage.jsx'
 import Navbar from './Components/Navbar.jsx'
 import Footer from './Components/Footer.jsx'
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Navbar/>
       <Routes>
               <Route path='/' element={<HomePage/>} />
+              <Route path='/about-me' element={<AboutMePage/>}/>
       </Routes>
     <Footer/>
     </>
