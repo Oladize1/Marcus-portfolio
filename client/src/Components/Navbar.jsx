739f1875983b865fef6ad847d032308ab1f96534
Marcus <pamoladize10@gmail.com>
2026-03-31 19:28:04 +0100

    Fix: icons on client and load static data on Homepage

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FiMenu } from 'react-icons/fi'
 const Navbar = () => {
   return (
     <header className='flex items-center justify-between px-3 py-6 md:p-10 flex-wrap'>
@@ -22,19 +23,7 @@ const Navbar = () => {
   <div className="drawer-content">
     {/* Page content here */}
     <label htmlFor="my-drawer-4" className="drawer-button btn btn-secondary">
-      <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block h-6 w-6 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
+<FiMenu className="inline-block h-6 w-6 stroke-current" />
     </label>
   </div>
   <div className="drawer-side z-30">
